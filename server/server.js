82660d08d970cfc5abe4061ac20a8f72f5372415
박기수 <gisu130@gmail.com>
2022-11-14 19:42:40 +0900

    first commit

---
+++ server/server.js
@@ -0,0 +1,141 @@
+const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
+
+// ================== DB연결 수행 전 라이브러리 호출 ========================
+const mysql = require("mysql2");
+const db = mysql.createPoolCluster();
+// ================== DB연결 수행 전 라이브러리 호출 ========================
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use(
+  session({
+    secret: "SECRET",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(
+  cors({
+    origin: true,
+    credentials: true, // 도메인 달라도 쿠키 공유 받아줄게
+  })
+);
+
+db.add("article_project", {
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "article_project",
+  port: 3306,
+});
+
+function 디비실행(query) {
+  return new Promise(function (resolve, reject) {
+    db.getConnection("article_project", function (error, connection) {
+      if (error) {
+        console.log("디비 연결 오류", error);
+        reject(true);
+      }
+
+      connection.query(query, function (error, data) {
+        if (error) {
+          console.log("쿼리 오류", error);
+          reject(true);
+        }
+
+        resolve(data);
+      });
+
+      connection.release();
+    });
+  });
+}
+
+app.get("/", async (req, res) => {
+  const 데이터 = await 디비실행("SELECT * FROM user");
+
+  console.log(데이터);
+
+  res.send("여기로 옵니다!");
+});
+
+/**
+ * 벨로퍼트 자바스크립트 (필수) ==============================================
+ */
+app.post("/join", async (req, res) => {
+  const { id, pw } = req.body;
+
+  const result = {
+    code: "success",
+    message: "회원가입되었습니다",
+  };
+
+  /**
+   * 아이디 중복체크
+   * - DB user테이블에 진짜 이 아이디가 존재하는지 확인해야함 !!
+   * 구현해주세요
+   */
+  const 회원 = await 디비실행(`SELECT * FROM user WHERE id = '${id}' `);
+
+  if (회원.length > 0) {
+    result.code = "error";
+    result.message = "이미 같은 아이디로 회원가입 되어있습니다";
+    res.send(result);
+    return;
+  }
+
+  // Mysql user 테이블에 INSERT 해줘야함 !!
+  const query = `INSERT INTO user(id,password,nickname) VALUES('${id}','${pw}','지나가던나그네')`;
+  await 디비실행(query);
+
+  res.send(result);
+});
+
+app.get("/user", (req, res) => {
+  res.send(req.session.loginUser);
+});
+
+app.get("/test", (req, res) => {
+  console.log(req.session);
+
+  res.send("//");
+});
+
+app.post("/login", async (req, res) => {
+  const { id, pw } = req.body;
+
+  /**
+   * 1. 아이디랑 비밀번호가 같은 데이터가 있는 확인 (Mysql DB)
+   * - 같은 데이터가 존재하면 session 저장
+   * - 없을 경우 [회원 정보가 존재하지 않습니다] 메시지 보내주기~
+   */
+
+  const result = {
+    code: "success",
+    message: "로그인 되었습니다",
+  };
+
+  const 회원 = await 디비실행(
+    `SELECT * FROM user WHERE id='${id}' AND password='${pw}'`
+  );
+
+  if (회원.length === 0) {
+    result.code = "error";
+    result.message = "회원 정보가 존재하지 않습니다";
+    res.send(result);
+    return;
+  }
+
+  req.session.loginUser = 회원[0];
+  req.session.save();
+
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log("서버가 시작되었습니다");
+});
--- src/App.js
+++ src/App.js
@@ -1,24 +1,150 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import "./App.css";
+
+// 도메인 달라도 쿠키공유해줄게
+axios.defaults.withCredentials = true;
+
+function Join() {
+  const navigation = useNavigate();
+
+  // useState는 항상위
+  const [data, setData] = React.useState({
+    id: "",
+    pw: "",
+  });
+
+  const 데이터변경 = (event) => {
+    const name = event.target.name;
+    const cloneData = { ...data };
+    cloneData[name] = event.target.value;
+    setData(cloneData);
+  };
+
+  const 회원가입 = async () => {
+    /**
+     * GET : 가져올떄
+     * POST : 뭐를 생성할떄
+     */
+    await axios({
+      url: "http://localhost:4000/join",
+      method: "POST",
+      data: data,
+    })
+      .then((res) => {
+        const { code, message } = res.data;
+
+        if (code === "success") {
+          alert(message);
+          navigation("/login");
+        }
+      })
+      .catch((e) => {
+        console.log("join Error", e);
+      });
+
+    // 여기서 내가 입력한 값을 console.log로 확인해주세요
+  };
+
+  return (
+    <div>
+      <input type="text" name="id" onChange={데이터변경} />
+      <input type="password" name="pw" onChange={데이터변경} />
+      <button type="button" onClick={회원가입}>
+        회원가입
+      </button>
+    </div>
+  );
+}
+
+function Login() {
+  const navigation = useNavigate();
+
+  const [data, setData] = React.useState({
+    id: "",
+    pw: "",
+  });
+
+  const 데이터변경 = (event) => {
+    const name = event.target.name;
+    const cloneData = { ...data };
+    cloneData[name] = event.target.value;
+    setData(cloneData);
+  };
+
+  const 로그인 = async () => {
+    await axios({
+      url: "http://localhost:4000/login",
+      method: "POST",
+      data: data,
+    })
+      .then((res) => {
+        alert(res.data.message);
+
+        if (res.data.code === "success") {
+          window.location.href = "/";
+        }
+      })
+      .catch((e) => {
+        console.log("로그인에러", e);
+      });
+  };
+
+  return (
+    <div>
+      <input name="id" onChange={데이터변경} />
+      <input name="pw" onChange={데이터변경} />
+      <button type="button" onClick={로그인}>
+        로그인
+      </button>
+    </div>
+  );
+}
+
+function Main() {
+  return <div>Main</div>;
+}
+
+/**
+ * 1. React Router DOM (완료)
+ * npm install react-router-dom
+ *
+ * 2. Context Provider (완료)
+ * 두개 설정해주세요
+ * https://to2.kr/d6P
+ */
+const StoreContext = React.createContext({});
 
 function App() {
+  const [loginUser, setLoginUser] = React.useState({});
+
+  const 세션정보가져오기 = async () => {
+    await axios({
+      url: "http://localhost:4000/user",
+    }).then((res) => {
+      console.log(res.data);
+
+      setLoginUser(res.data);
+    });
+  };
+
+  React.useEffect(() => {
+    세션정보가져오기();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreContext.Provider
+      value={{
+        loginUser,
+      }}
+    >
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/join" element={<Join />} />
+      </Routes>
+    </StoreContext.Provider>
   );
 }
 
